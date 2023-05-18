5f76e85970e9a30ff29318004c347926e588dfa9
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-05-18 14:30:50 +0530

    upd cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const cors = require('cors');
+
 require("dotenv").config();
 const app =express()
 const port =process.env.PORT
 const mongoDB=require("./db");
 mongoDB();
+app.use(cors());
 
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
