08cebe4880ff432d4716a49970c4e4566dc10b3d
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-05-20 15:08:22 +0530

    upd

--- backend/routes/DisplayData.js
+++ backend/routes/DisplayData.js
@@ -5,7 +5,9 @@ const router =express.Router()
 router.post("/food-data",(req,res)=>{
     try{
         // console.log(global.foodData);
-        res.send([global.foodData ,global.foodCategory])
+        // res.send([global.foodData ,global.foodCategory])
+        res.json("data send")
+
     }catch(err){
         console.log(err.message);
         req.send("Server Error")
